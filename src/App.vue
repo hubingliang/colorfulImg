5cf7c4842fb0ef90b592a69f25ebe2e959877947
BrownHu <1104524352@qq.com>
2018-03-28 00:50:33 +0800

    取色功能完成

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <HomePage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HomePage from './components/HomePage'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+  overflow: hidden;
 }
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,166 @@
+<template>
+  <div class="homePage">
+    <div class="imgBox">
+      <img class="img" :src='imgUrl' v-show="imgShow" @click="getImg" id="img" crossorigin="anonymous">
+      <div class="loader" v-show="loading">
+        <div class="loader-inner">
+          <div class="loader-line-wrap">
+            <div class="loader-line"></div>
+          </div>
+          <div class="loader-line-wrap">
+            <div class="loader-line"></div>
+          </div>
+          <div class="loader-line-wrap">
+            <div class="loader-line"></div>
+          </div>
+          <div class="loader-line-wrap">
+            <div class="loader-line"></div>
+          </div>
+          <div class="loader-line-wrap">
+            <div class="loader-line"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="color">
+      <img src="https://i.loli.net/2018/03/27/5aba29b5b97ae.png" alt="" class="author">
+      <div class="title">ColorfulImg</div>
+      <div class="describe">← Click it to get the main color from the picture</div>
+      <div class="Dominant">Dominant Color :</div>
+      <div id="color">{{ rgb }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  mounted(){
+    this.getImg()
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      imgUrl: 'https://picsum.photos/580/600/?image=23',
+      img: null,
+      loading: true,
+      imgShow: false,
+      rgb: null,
+    }
+  },
+  methods:{
+    getImg: function(){
+      let i = Math.floor(Math.random()*(100+1))
+      let color = document.getElementById('color')
+      this.img = document.getElementById('img')
+      this.imgUrl = `https://picsum.photos/580/600/?image=${i}`
+      this.loading = true
+      this.imgShow = false
+      this.img.onload =()=> {
+        this.loading = false
+        this.imgShow = true
+        let rgb = this.colorfulImg(this.img)
+        this.rgb = 'rgb('+rgb.r+','+rgb.g+','+rgb.b+')'
+        document.body.style.backgroundColor = this.rgb
+        color.style.background = this.rgb
+        color.style.color = this.rgb
+        this.copyColor()
+      }
+      
+      
+    },
+    colorfulImg: function(imgEl){
+      var blockSize = 5, // only visit every 5 pixels
+          defaultRGB = {r:0,g:0,b:0}, // for non-supporting envs
+          canvas = document.createElement('canvas'),
+          context = canvas.getContext && canvas.getContext('2d'),
+          data, width, height,
+          i = -4,
+          length,
+          rgb = {r:0,g:0,b:0},
+          count = 0;
+      if (!context) {
+          return defaultRGB;
+      }
+      height = canvas.height = imgEl.naturalHeight || imgEl.offsetHeight || imgEl.height;
+      width = canvas.width = imgEl.naturalWidth || imgEl.offsetWidth || imgEl.width;
+      context.drawImage(imgEl, 0, 0);
+      try {
+          data = context.getImageData(0, 0, width, height);
+      } catch(e) {
+          /* security error, img on diff domain */alert('x');
+          return defaultRGB;
+      }
+      console.log
+      length = data.data.length;
+      while ( (i += blockSize * 4) < length ) {
+          ++count;
+          rgb.r += data.data[i];
+          rgb.g += data.data[i+1];
+          rgb.b += data.data[i+2];
+      }
+
+      // ~~ used to floor values
+      rgb.r = ~~(rgb.r/count);
+      rgb.g = ~~(rgb.g/count);
+      rgb.b = ~~(rgb.b/count);
+
+      return rgb;
+    },
+    copyColor: function(){
+      let rgb = document.getElementById("color").innerHTML
+      var oInput = document.createElement('input');
+      oInput.value = rgb;
+      document.body.appendChild(oInput);
+      oInput.select(); // 选择对象
+      document.execCommand("Copy"); // 执行浏览器复制命令
+      oInput.className = 'oInput';
+      oInput.style.display='none';
+      console.log(rgb)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+.homePage{
+  height: 600px;
+  width: 1020px;
+  display: flex;
+  .imgBox{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 580px;
+    height: 600px;
+  }
+  .color{
+    background: rgb(253, 245, 237);
+    flex: 1;
+    padding: 60px 30px 60px 40px;
+    .author{
+      width: 70px;
+      height: 70px;
+      border-radius: 50%;
+    }
+    .title{
+      font-size: 40px;
+      font-weight: bold;
+      margin: 50px 0;
+    }
+    .describe{
+      margin-bottom: 20px;
+    }
+    .Dominant{
+
+    }
+    #color{
+      margin-top: 20px;
+      width: 100px;
+      height: 50px;
+      overflow: hidden;
+    }
+  }
+}
+</style>
