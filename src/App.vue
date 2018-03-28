cb859f9f0769eaa830dd3cfb3cf478066b3d572e
BrownHu <1104524352@qq.com>
2018-03-28 12:36:35 +0800

    随机图片API 加载失败问题解决

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,12 @@ export default {
   padding: 0;
   box-sizing: border-box;
 }
+.icon {
+  width: 1em; height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
 #app {
   display: flex;
   justify-content: center;
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -23,6 +23,11 @@
       </div>
     </div>
     <div class="color">
+      <a href="https://github.com/hubingliang/colorfulImg" target="_blank">
+        <svg class="icon" aria-hidden="true">
+            <use xlink:href="#icon-GitHub"></use>
+        </svg>
+      </a>
       <img src="https://i.loli.net/2018/03/27/5aba29b5b97ae.png" alt="" class="author">
       <div class="title">ColorfulImg</div>
       <div class="describe">← Click it to get the main color from the picture</div>
@@ -41,7 +46,7 @@ export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      imgUrl: 'https://picsum.photos/580/600/?image=23',
+      imgUrl: '',
       img: null,
       loading: true,
       imgShow: false,
@@ -50,13 +55,14 @@ export default {
   },
   methods:{
     getImg: function(){
-      let i = Math.floor(Math.random()*(100+1))
+      let i = Math.floor(Math.random()*(1000+1))
       let color = document.getElementById('color')
       this.img = document.getElementById('img')
       this.imgUrl = `https://picsum.photos/580/600/?image=${i}`
+      //this.imgUrl = `https://picsum.photos/580/600?random`
       this.loading = true
       this.imgShow = false
-      this.img.onload =()=> {
+      this.img.onload = ()=> {
         this.loading = false
         this.imgShow = true
         let rgb = this.colorfulImg(this.img)
@@ -65,6 +71,10 @@ export default {
         color.style.background = this.rgb
         color.style.color = this.rgb
         this.copyColor()
+      }
+      this.img.onerror = ()=>{
+        console.log('error')
+        this.getImg()
       }
       
       
@@ -116,7 +126,6 @@ export default {
       document.execCommand("Copy"); // 执行浏览器复制命令
       oInput.className = 'oInput';
       oInput.style.display='none';
-      console.log(rgb)
     }
   }
 }
@@ -139,6 +148,14 @@ export default {
     background: rgb(253, 245, 237);
     flex: 1;
     padding: 60px 30px 60px 40px;
+    position: relative;
+    .icon{
+      position: absolute;
+      right: 20px;
+      bottom: 20px;
+      width: 30px;
+      height: 30px;
+    }
     .author{
       width: 70px;
       height: 70px;
